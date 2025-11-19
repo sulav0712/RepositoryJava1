b6f61affac1e5430ddcd697e8d43d5b764070c69
Sulav Shrestha <sulavshres24@gmail.com>
2025-11-19 10:51:53 +0545

    question 3

---
+++ DataTypeInspector.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class DataTypeInspector here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DataTypeInspector {
+    public static void main(String[] args) {
+
+        byte byteValue = 100;
+
+        short shortValue = 20000;
+
+        int intValue = 150000;
+
+        long longValue = 5000000000L;
+
+        float floatValue = 5.75F;
+
+        double doubleValue = 19.98765;
+
+        char charValue = 'A';
+
+        boolean booleanValue = true;
+
+        System.out.println("Byte Value: " + byteValue);
+        System.out.println("Short Value: " + shortValue);
+        System.out.println("Int Value: " + intValue);
+        System.out.println("Long Value: " + longValue);
+        System.out.println("Float Value: " + floatValue);
+        System.out.println("Double Value: " + doubleValue);
+        System.out.println("Char Value: " + charValue);
+        System.out.println("Boolean Value: " + booleanValue);
+    }
+}
