f2672455db874d985b36a4903829ba8d4dd5dc61
Sulav Shrestha <sulavshres24@gmail.com>
2025-11-19 10:07:45 +0545

    done

--- WorkshopC111.java
+++ WorkshopC111.java
@@ -13,11 +13,11 @@ static int qty; //static variable
 public static void main(String[] args)
 { //<className><variable> = new<className>()
     int age =18;
-    WorkshopC1 v1 = new WorkshopC111(); //calling instance variable in static method
+    WorkshopC111 v1 = new WorkshopC111(); //calling instance variable in static method
     System.out.println(v1.age);
     
     System.out.println(age);
-    System.out.println(WorkshopC11.qty);
+    System.out.println(WorkshopC111.qty);
     
 } 
     
